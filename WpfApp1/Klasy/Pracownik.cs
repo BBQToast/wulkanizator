b5b9da38f6935e67d8220e33c1fc5ba0c050f5c3
Koname <apkroczek@gmail.com>
2020-03-15 22:19:58 +0100

    Making Db connestion

--- WpfApp1/Klasy/Pracownik.cs
+++ WpfApp1/Klasy/Pracownik.cs
@@ -7,8 +7,7 @@ using System.Runtime.Serialization.Formatters.Binary;
 
 namespace WpfApp1.Klasy
 {
-    [Serializable()]
-    public class Pracownik : ISerializable
+    public class Pracownik
     {
         public int ID { get; set; }
         public string Imie { get ; set; }
@@ -36,29 +35,7 @@ namespace WpfApp1.Klasy
 
             return tmp;
         }
-        public void GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-            info.AddValue("id_pracownika", ID);
-            info.AddValue("Imie", Imie);
-            info.AddValue("Nazwisko", Nazwisko);
-            info.AddValue("Mail", Mail);
-            info.AddValue("telefon", telefon);
-            info.AddValue("pesel", pesel);
-            info.AddValue("data_rozpoczecia", Data_zatrudnienia);
-            info.AddValue("data_zakonczenia", Data_zakonczenia_pracy);
-        }
 
-        public Pracownik(SerializationInfo info, StreamingContext ctxt)
-        {
-            ID = (int)info.GetValue("id_pracownika", typeof(int));
-            Imie = (string)info.GetValue("Imie", typeof(string));
-            Nazwisko = (string)info.GetValue("Nazwisko", typeof(string));
-            Mail = (string)info.GetValue("Mail", typeof(string));
-            telefon = (string)info.GetValue("telefon", typeof(string));
-            pesel = (int)info.GetValue("pesel", typeof(int));
-            Data_zatrudnienia = (DateTime)info.GetValue("data_zakonczenia", typeof(DateTime));
-            Data_zakonczenia_pracy = (DateTime)info.GetValue("data_zakonczenia", typeof(DateTime));
-        }
     }
 
 }
